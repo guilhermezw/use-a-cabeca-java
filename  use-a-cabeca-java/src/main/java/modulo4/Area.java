4f71bdfd2981e3e97473665b645bbc2a5b25a444
Guilherme <guifefo@gmail.com>
2026-01-17 18:46:22 -0300

    .

---
+++  use-a-cabeca-java/src/main/java/modulo4/Area.java
@@ -0,0 +1,10 @@
+package modulo4;
+
+public class Area {
+    int height;
+    int width;
+
+    int calcArea(int height, int width) {
+        return height * width;
+    }
+}
---
+++  use-a-cabeca-java/src/main/java/modulo4/Value.java
@@ -0,0 +1,13 @@
+package modulo4;
+
+public class Value {
+    int intValue;
+
+    public int doStuff(int factor){
+        if (intValue > 100){
+            return intValue * factor;
+        } else {
+            return intValue * ( 5 - factor);
+        }
+    }
+}
